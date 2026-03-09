195d9a1d57ebf353f4cd11568b740238aaaa50d4
Matthew Chng <matthewchng@gmail.com>
2026-03-09 18:48:44 +0800

    chore: bump version to 1.0.0-beta.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "sg.org.bcc.attendance"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0-beta.1"
+        versionCode = 3
+        versionName = "1.0.0-beta.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
